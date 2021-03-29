6226a03e0ca3ee1e2141219609743e8838515183
nonusae <nonusae@gmail.com>
2021-03-29 15:38:31 +0700

    Polish more progress bar style

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -31,26 +31,41 @@ const SIZES  = {
 }
 
 const BaseWrapper = styled.div`
-  width: 370px;
   height: var(--height);
-  background-color: ${COLORS.gray50};
+  background-color: ${COLORS.transparentGray15};
   border-radius: var(--borderRadius);
   padding: var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  /*  Trim off corner when progress bar is full */
+  overflow: hidden;
 `
 
-const Fill = styled.div`
+const Bar = styled.div`
   height: 100%;
   width: ${({value}) => Rounded(value)}%;
   background: ${COLORS.primary};
-  border-radius: 4px;
-  border-top-right-radius: ${({value}) => Rounded(value) === 100 ? '4px': '0px'};
-  border-bottom-right-radius: ${({value}) => Rounded(value) === 100 ? '4px': '0px'};
+  border-radius: 4px 0px 0px 4px;
+  border-top-right-radius: ${({value}) => Rounded(value) === 100 && '4px'};
+  border-bottom-right-radius: ${({value}) => Rounded(value) === 100 && '4px'};
 `
 
 const ProgressBar = ({ value, size }) => {
   const styles = SIZES[size]
-  return <BaseWrapper style={styles} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
-    <Fill value={value}></Fill>
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to Progressbar: ${size}`)
+  }
+
+  return <BaseWrapper
+    style={styles}
+    role="progressbar"
+    aria-valuenow={value}
+    aria-valuemin="0"
+    aria-valuemax="100"
+  >
+    <VisuallyHidden>{value}%</VisuallyHidden>
+    <Bar value={value}></Bar>
   </BaseWrapper>;
 };
 
